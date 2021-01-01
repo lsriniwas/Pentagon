d95973ffd47e572ef88e4f917a6892f83ad428c5
Amal Biju <amalbiju99@gmail.com>
2021-01-01 16:46:07 +0530

    Job Listing - UI & Job search complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import JobListing from './Components/JobListing/JobListing';
 import { Routes } from './Routes/Routes';
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-     {/* <Routes/> */}
-     <JobListing/>
+     <Routes/>
     </div>
   );
 }
